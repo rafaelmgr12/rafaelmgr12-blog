bb7032139a05c5310e11a1071feeed37adccfd0b
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-09-23 11:40:51 -0300

    feat: add styles

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My Personal Notes - Rafael Ribeiro';
-export const SITE_DESCRIPTION = 'Here are all my notes on software engineering, software development and a few more things';
+export const SITE_TITLE = 'A Journal about Software - Rafael Ribeiro';
+export const SITE_DESCRIPTION = 'My daily pills about my journey as a software engineer.';
 export const HOMEPAGE_URL = "http://rafaelr.dev"

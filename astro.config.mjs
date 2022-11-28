d4cac0e6550347017e77d7ecdc412c75a8a22c7c
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-11-28 09:01:32 -0300

    feat: add new styles to the markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,18 @@ export default defineConfig({
           h6: 'font-bold font-mplus',
           img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
           p: 'mb-6',
-          a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500'
+          a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500',
+          li: 'mb-2 font-mplus',
+          ul: 'list-disc list-inside',
+          ol: 'list-decimal list-inside',
+          blockquote: 'border-l-4 border-orange-500 pl-4',
+          code: 'bg-slate-100 dark:bg-zinc-800 rounded-md p-1',
+          pre: 'bg-slate-100 dark:bg-zinc-800 rounded-md p-4',
+          table: 'table-auto border-collapse border border-slate-300 dark:border-zinc-700',
+          th: 'border border-slate-300 dark:border-zinc-700',
+          td: 'border border-slate-300 dark:border-zinc-700',
+          
+
         }
       ]
     ]
